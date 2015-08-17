ba4d4c390990277f21942987b8d97e98e51d2d24
nigelfloe <dwr229@gmail.com>
2015-08-16 21:09:41 -0400

    collect lots of tweets from user

--- bin/run.rb
+++ bin/run.rb
@@ -2,26 +2,30 @@ require_relative '../config/environment'
 
 twitter = TwitterApi.new
 # File.open("./lib/tweets.txt", "r+") {|x| x.write(p twitter.collect_tweets_from("taylorswift13"))}
+user = "townsendpaige"
+# puts twitter.get_name(user)
+array = twitter.get_all_tweets(user).map { |tweet| "#{tweet.text}"  }
 
-array = twitter.collect_tweets_from("taylorswift13")
-
-tweet = array.sample
-
-
-usernames = tweet.scan(/@(\w+)/)
-remainder = tweet.split(/@(\w+)/)
-
-names = usernames.map do |username|
-  username = twitter.get_name(username)
-end
-
-swapped = remainder.map do |section|
-  if usernames.include?([section])
-    index = usernames.index([section])
-    section = names[index]
-  else
-    section
-  end
-end
-
-puts swapped.join
+# array = twitter.collect_tweets_from(user)
+# binding.pry
+#
+# tweet = array.sample
+#
+#
+# usernames = tweet.scan(/@(\w+)/)
+# remainder = tweet.split(/@(\w+)/)
+#
+# names = usernames.map do |username|
+#   username = twitter.get_name(username)
+# end
+#
+# swapped = remainder.map do |section|
+#   if usernames.include?([section])
+#     index = usernames.index([section])
+#     section = 'you!'
+#   else
+#     section
+#   end
+# end
+#
+# puts swapped.join
